44b60ed29c6f6e261aa77f338d79eae8ae9f36bb
강원경 <kokk012@naver.com>
2022-08-03 17:57:27 +0900

    #3 재커밋

--- Week01/BOJ 1991/Main_1991_KWG.java
+++ Week01/BOJ 1991/Main_1991_KWG.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 public class Main_1991_KWG {
 	static char[][] tree;
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+ 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
